37da107e4965de6dc82ac58550a4cccb81aa85e6
RJose94 <137239149+RJose94@users.noreply.github.com>
2023-08-03 22:32:09 -0500

    Parte 3

--- proyectorintegrador.py
+++ proyectorintegrador.py
@@ -1,22 +1,14 @@
-import readchar
-from readchar import readkey, key
-def Juego():
-    d=0
-    jueg=True
-    while d==0:
-        print(f"Bienvenido al juego del laberinto, escribe tu nombre para comenzar")
-    nombre=input()
-    if nombre =="1":
-        d=1
-    else:
-        print(f"Escribe algo aleatorio")
-        while jueg == True:
-            tecla = readchar.readkey()
-            teclaespecial= readkey()
-            print(tecla, end="")
-            if teclaespecial == key.UP:
-                print(f"Gracias por jugar")
-                break
+import os, readchar
+from readchar import readkey
+def borrar_terminal():
+    cont=0
+    print("Bienvenido a borrar ternminal")
+    while cont <50:
+        tecla = readchar.readkey()
+        if ord(tecla)>0:
+            os.system('cls' if os.name=='nt' else 'clear')
+            cont=cont+1
+            print(cont)
 
 if __name__ == "__main__":
-    Juego()
+    borrar_terminal()
